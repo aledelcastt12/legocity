4b1c743688a300c752a3ee5c76df0572fc487b24
Alejandra Del Castillo <maledelcastillo123@gmail.com>
2021-10-06 12:10:49 +0200

    ok

--- merged-tele12/routes/index.js
+++ merged-tele12/routes/index.js
@@ -1,3 +1,16 @@
 const { router } = require("express");
 const nodemailer = require("nodemailer");
 const router = router();
+
+router.post("/send-email", async (req, res) => {
+  const { email, message } = req.body;
+
+  contentHTML = `
+<h1> User Iformation</h1>
+ <ul> 
+ <li>UserEmail: ${email} </li> 
+ </ul>
+ <p> ${message} </p> 
+ `;
+  console.log("proceso completado");
+});
