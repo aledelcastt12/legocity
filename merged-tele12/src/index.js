f1f31369f47c2029468d5299b0a67adae4b82146
Alejandra Del Castillo <maledelcastillo123@gmail.com>
2021-10-07 09:47:12 +0200

    Update index.js

--- merged-tele12/src/index.js
+++ merged-tele12/src/index.js
@@ -2,4 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.urloncoded);
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use("./routes/index");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listener(5050, () => {
+  console.log("server on port 5050");
+});
