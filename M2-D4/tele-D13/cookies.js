a87d4b637c87aeb3e3a8ffb6c9b60615cc28c659
Alejandra Del Castillo <maledelcastillo123@gmail.com>
2021-10-13 10:58:16 +0200

    Update cookies.js

--- M2-D4/tele-D13/cookies.js
+++ M2-D4/tele-D13/cookies.js
@@ -2,4 +2,6 @@ const btn_close = docuemnt.querySelector("#close");
 const cont_cookies = document.querySelector(".cookie-box");
 const modal = docuemnt.querySelector(".modal");
 const ver_pol = document.querySelector("#ver_pol");
-const c_modal = document.querySelector("close_modal");
+const c_modal = document.querySelector("#close_modal");
+const ac_cookie = document.querySelector("#aceptar_cookie");
+const ac_cookie2 = document.querySelector("#aceptar_modal");
